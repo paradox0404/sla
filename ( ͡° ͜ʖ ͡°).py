b65765b4c73b1225ee2a390fcfdd2b0a5bc7d9f0
paradox0404 <85161414+paradox0404@users.noreply.github.com>
2021-06-01 06:05:38 -0300

    Add files via upload

---
+++ ( ͡° ͜ʖ ͡°).py
@@ -0,0 +1,18 @@
+print ("sejam bem vindos a minha ferramenta test eu (paradox) fiz essa ferramenta de fans para fans espero que aprveitem")
+
+
+print ("MELHORES SITES")
+
+print ("xvideos.com")
+
+print ("pornhub.com")
+
+print ("ATRIZES QUE RECOMENDO")
+
+print ("https://pt.pornhub.com/pornstar/lasirena69")
+
+print ("https://pt.pornhub.com/model/purple-bitch")
+
+print ("https://pt.pornhub.com/pornstar/sasha-grey")
+
+print ("então e so isso fml,na proxima versão sera + interessante,BOA BRONHA( ͡° ͜ʖ ͡°)")
